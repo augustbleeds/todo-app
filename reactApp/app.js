5c8cc28dbad153ff3ea0341345523b7da1200806
Augustus Chang <augustus.chang@rutgers.edu>
2017-06-30 13:50:10 -0700

    added strike

--- reactApp/app.js
+++ reactApp/app.js
@@ -4,22 +4,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var dummyData = ["Go to sleep", "Eat burgers", "Blah"];
+var dummyData = [{taskText: "Go to Sleep", completed: false}, {taskText: "Eat burgers", completed: false}, {taskText: "lah", completed: true}];
+
+
 
 class TodoList extends React.Component{
   render(){
     return (
-      <ul>
-        {dummyData.map((item)=>(<Todo task={item}/>))}
-      </ul>
+      <div>
+        <InputLine />
+        <ul>
+          {dummyData.map((item)=>(<Todo task={item}/>))}
+        </ul>
+      </div>
     )
   }
 }
 
 class Todo extends React.Component{
   render(){
-    return (
-      <li><button>X</button>{this.props.task}</li>
+    return this.props.task.completed ? (<li><button>X</button><strike>{this.props.task.taskText}</strike></li>) : (<li><button>X</button>{this.props.task.taskText}</li>) 
+  }
+}
+
+class InputLine extends React.Component{
+  render(){
+    return(
+      <div>
+        <input type="text" placeholder="task"/> <input type="submit" value="Add todo"/>
+      </div>
     )
   }
 }

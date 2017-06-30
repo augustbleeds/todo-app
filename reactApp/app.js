1f9f17fcc8905e0cbc11911f2812068e132c0a01
Augustus Chang <augustus.chang@rutgers.edu>
2017-06-30 14:15:20 -0700

    Made todo into a state in TodoApp

--- reactApp/app.js
+++ reactApp/app.js
@@ -6,36 +6,53 @@ import ReactDOM from 'react-dom';
 
 var dummyData = [{taskText: "Go to Sleep", completed: false}, {taskText: "Eat burgers", completed: false}, {taskText: "lah", completed: true}];
 
+class TodoApp extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: []
+    }
+  }
 
+  componentDidMount(){
+    this.setState({todos: dummyData})
+  }
+
+  render(){
+    return(
+      <div>
+        <InputLine />
+        <TodoList todos={this.state.todos} />
+      </div>
+    )
+  }
+}
 
 class TodoList extends React.Component{
   render(){
     return (
-      <div>
-        <InputLine />
         <ul>
-          {dummyData.map((item)=>(<Todo task={item}/>))}
+          {this.props.todos.map((item)=>(<Todo task={item}/>))}
         </ul>
-      </div>
     )
   }
 }
 
 class Todo extends React.Component{
   render(){
-    return this.props.task.completed ? (<li><button>X</button><strike>{this.props.task.taskText}</strike></li>) : (<li><button>X</button>{this.props.task.taskText}</li>) 
+    return this.props.task.completed ? (<li><button className="btn btn-default">X</button><strike>{' ' + this.props.task.taskText}</strike></li>) : (<li><button className="btn btn-default">X</button>{' ' + this.props.task.taskText}</li>)
   }
 }
 
 class InputLine extends React.Component{
   render(){
     return(
-      <div>
-        <input type="text" placeholder="task"/> <input type="submit" value="Add todo"/>
-      </div>
+      <form className="form-inline">
+        <input className="form-control mb-2 mr-sm-2 mb-sm-0" type="text" placeholder="task"/> <input className="btn btn-primary" type="submit" value="Add todo"/>
+      </form>
     )
   }
 }
 
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(<TodoApp />, document.getElementById('root'));

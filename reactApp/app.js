0da56280f0929a48d944a4a51dd22f62705e85ec
Augustus Chang <augustus.chang@rutgers.edu>
2017-06-30 13:40:45 -0700

    it told us to commit

--- reactApp/app.js
+++ reactApp/app.js
@@ -4,4 +4,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p> Donovan ! </p>, document.getElementById('root'));
+var dummyData = ["Go to sleep", "Eat burgers", "Blah"];
+
+class TodoList extends React.Component{
+  render(){
+    return (
+      <ul>
+        {dummyData.map((item)=>(<Todo task={item}/>))}
+      </ul>
+    )
+  }
+}
+
+class Todo extends React.Component{
+  render(){
+    return (
+      <li><button>X</button>{this.props.task}</li>
+    )
+  }
+}
+
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
